f222cbb0282b9b99e5779a48417d3cc287d8b170
OatmealDome <theoatmealdome@gmail.com>
2019-03-28 13:36:59 -0400

    InteractiveMessageCleanupJob: Move to BcatBotFramework namespace

--- BcatBotFramework/Scheduler/Job/InteractiveMessageCleanupJob.cs
+++ BcatBotFramework/Scheduler/Job/InteractiveMessageCleanupJob.cs
@@ -2,10 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Quartz;
 using BcatBotFramework.Social.Discord;
+using Quartz;
 
-namespace SmashBcatDetector.Scheduler.Job
+namespace BcatBotFramework.Scheduler.Job
 {
     public class InteractiveMessageCleanupJob : IJob
     {
