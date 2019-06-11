45d4316494efba89ef52c03115e56aa8edd258b2
OatmealDome <theoatmealdome@gmail.com>
2019-06-11 02:59:02 -0400

    Localizations: Add Patreon links to URL localizables

--- SmashBcatDetector/Social/Discord/Command/DebugCommand.cs
+++ SmashBcatDetector/Social/Discord/Command/DebugCommand.cs
@@ -15,6 +15,8 @@ using BcatBotFramework.Social.Twitter;
 using SmashBcatDetector.Difference.Handlers.Twitter;
 using System.Linq;
 using BcatBotFramework.Json;
+using Nintendo.Bcat;
+using Nintendo.SmashUltimate.Bcat.LineNewsData;
 
 namespace SmashBcatDetector.Social.Discord.Command
 {
@@ -42,7 +44,46 @@ namespace SmashBcatDetector.Social.Discord.Command
 
             //TwitterManager.GetAccount("SSBUBot").Tweet("[Test]", "test tweet from debug command", "URL: https://google.com");
             
-            LineNewsTwitterHandler.HandleAdded(ContainerCache.GetLineNews().Last());
+            //LineNewsTwitterHandler.HandleAdded(ContainerCache.GetLineNews().Last());
+
+            /*IList<PopUpNews> newsList = ContainerCache.GetPopUpNews();
+            PopUpNews longestNews = newsList.FirstOrDefault();
+            foreach (PopUpNews news in newsList)
+            {
+                if (news.TitleText[Language.EnglishUS].Length > longestNews.TitleText[Language.EnglishUS].Length)
+                {
+                    longestNews = news;
+                }
+            }
+
+            await Context.Channel.SendMessageAsync("[Debug] tweeting " + longestNews.Id);
+
+            PopUpNewsTwitterHandler.HandleAdded(longestNews);*/
+
+            /*IList<LineNews> newsList = ContainerCache.GetLineNews();
+
+            LineNews longestNews = newsList.FirstOrDefault();
+            OneLine longestLine = longestNews.OneLines[0];
+
+            foreach (LineNews news in newsList)
+            {
+                foreach (OneLine line in news.OneLines)
+                {
+                    if (line.Text[Language.EnglishUS].Length > longestLine.Text[Language.EnglishUS].Length)
+                    {
+                        longestNews = news;
+                        longestLine = line;
+                    }
+                }
+            }
+
+            await Context.Channel.SendMessageAsync("[Debug] line news " + longestNews.Id + ", oneline " + longestLine.Id + " is " + longestLine.Text[Language.EnglishUS].Length);*/
+
+            await Context.Channel.SendMessageAsync("[Debug] Test tweets...");
+
+            PopUpNewsTwitterHandler.HandleAdded(ContainerCache.GetPopUpNews().FirstOrDefault());
+            PresentTwitterHandler.HandleAdded(ContainerCache.GetPresents().FirstOrDefault());
+            LineNewsTwitterHandler.HandleAdded(ContainerCache.GetLineNews().FirstOrDefault());
         }
 
         private static string ToJson(Nintendo.SmashUltimate.Bcat.Container container)
